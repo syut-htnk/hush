da2c7403679aba597d14f4d033d148554adbe15f
Shuto Hatanaka <aaaa.kanh@gmail.com>
2025-01-04 15:17:09 +0900

    投稿ページのカテゴリー区切りを変更

--- template-parts/content-page.php
+++ template-parts/content-page.php
@@ -11,7 +11,7 @@
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header">
-		<div class="entry-categories"><?php the_category( ', ' );?></div>
+		<div class="entry-categories"><?php the_category( ' | ' );?></div>
 		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
 		<div class="entry-meta"><?php lull_posted_on(); ?></div>
 		<div class="breadcrumb"><?php lull_get_breadcrumb(); ?></div>
