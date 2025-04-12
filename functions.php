e4c080f1f3b2e0c148e240513327c1e0f9f72344
Shuto Hatanaka <aaaa.kanh@gmail.com>
2025-04-12 14:34:31 +0900

    目次のデザインを少し変更

--- functions.php
+++ functions.php
@@ -326,72 +326,76 @@ function lull_get_sns_buttons()
  */
 function lull_add_index($content)
 {
-	if (is_single()) {
-		// 属性がある場合も考慮した正規表現
-		$pattern = '/<h[1-6](?:\s+[^>]*)?>(.+?)<\/h[1-6]>/s';
-		preg_match_all($pattern, $content, $elements, PREG_SET_ORDER);
+    if (is_single()) {
+        // 属性がある場合も考慮した正規表現
+        $pattern = '/<h[1-6](?:\s+[^>]*)?>(.+?)<\/h[1-6]>/s';
+        preg_match_all($pattern, $content, $elements, PREG_SET_ORDER);
 
-		if (count($elements) >= 1) {
-			$toc = '';
-			$i = 0;
-			$currentlevel = 0;
-			$id = 'chapter-';
+        if (count($elements) >= 1) {
+            $toc = '';
+            $i = 0;
+            $currentlevel = 0;
+            $id = 'chapter-';
 
-			foreach ($elements as $element) {
-				$id .= $i + 1;
-				$regex = '/' . preg_quote($element[0], '/') . '/su';
-				$replace_title = preg_replace('/<(h[1-6])(?:\s+[^>]*)?>(.+?)<\/(h[1-6])>/s', '<$1 id="' . $id . '">$2</$3>', $element[0], 1);
-				$content = preg_replace($regex, $replace_title, $content, 1);
+            foreach ($elements as $element) {
+                $id .= $i + 1;
+                $regex = '/' . preg_quote($element[0], '/') . '/su';
+                $replace_title = preg_replace('/<(h[1-6])(?:\s+[^>]*)?>(.+?)<\/(h[1-6])>/s', '<$1 id="' . $id . '">$2</$3>', $element[0], 1);
+                $content = preg_replace($regex, $replace_title, $content, 1);
 
-				if (strpos($element[0], '<h2') !== false) {
-					$level = 1;
-				} elseif (strpos($element[0], '<h3') !== false) {
-					$level = 2;
-				} elseif (strpos($element[0], '<h4') !== false) {
-					$level = 3;
-				} elseif (strpos($element[0], '<h5') !== false) {
-					$level = 4;
-				} elseif (strpos($element[0], '<h6') !== false) {
-					$level = 5;
-				}
+                if (strpos($element[0], '<h2') !== false) {
+                    $level = 1;
+                } elseif (strpos($element[0], '<h3') !== false) {
+                    $level = 2;
+                } elseif (strpos($element[0], '<h4') !== false) {
+                    $level = 3;
+                } elseif (strpos($element[0], '<h5') !== false) {
+                    $level = 4;
+                } elseif (strpos($element[0], '<h6') !== false) {
+                    $level = 5;
+                }
 
-				while ($currentlevel < $level) {
-					if (0 === $currentlevel) {
-						$toc .= '<ul class="index__list">';
-					} else {
-						$toc .= '<ul class="index__list_child">';
-					}
-					$currentlevel++;
-				}
+                while ($currentlevel < $level) {
+                    if (0 === $currentlevel) {
+                        $toc .= '<ul class="index__list">';
+                    } else {
+                        $toc .= '<ul class="index__list_child">';
+                    }
+                    $currentlevel++;
+                }
 
-				while ($currentlevel > $level) {
-					$toc .= '</li></ul>';
-					$currentlevel--;
-				}
+                while ($currentlevel > $level) {
+                    $toc .= '</li></ul>';
+                    $currentlevel--;
+                }
 
-				$toc .= '<li class="index__item"><a href="#' . $id . '" class="index__link">' . $element[1] . '</a>';
-				$i++;
-				$id = 'chapter-';
-			} // foreach
+                $toc .= '<li class="index__item"><a href="#' . $id . '" class="index__link">' . $element[1] . '</a>';
+                $i++;
+                $id = 'chapter-';
+            } // foreach
 
-			while ($currentlevel > 0) {
-				$toc .= '</li></ul>';
-				$currentlevel--;
-			}
+            while ($currentlevel > 0) {
+                $toc .= '</li></ul>';
+                $currentlevel--;
+            }
 
-			$index = '<div class="single__index index" id="toc"><div class="index__title">Contents</div>' . $toc . '</div>';
-			$h2 = '/<h2.*?>/i';
+            // インラインCSSを追加して目次の表示を改善
+            $inline_css = '<style>
+            </style>';
 
-			if (preg_match($h2, $content, $h2s)) {
-				$content = preg_replace($h2, $index . $h2s[0], $content, 1);
-			}
-		}
-	}
+            $index = '<div class="single__index index" id="toc"><div class="index__title">Contents</div>' . $inline_css . $toc . '</div>';
+            $h2 = '/<h2.*?>/i';
 
-	// 最後に <hr> を追加
-	$content .= '<hr> <p></p>';
+            if (preg_match($h2, $content, $h2s)) {
+                $content = preg_replace($h2, $index . $h2s[0], $content, 1);
+            }
+        }
+    }
 
-	return $content;
+    // 最後に <hr> を追加
+    $content .= '<hr> <p></p>';
+
+    return $content;
 }
 add_filter('the_content', 'lull_add_index');
 
